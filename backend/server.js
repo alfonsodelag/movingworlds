f47847aaf0f6464b8d3861f8e37787df6c646c92
Alfonso <alfonsodelag1@gmail.com>
2021-01-27 18:12:03 +0100

    getting registered and last_access dates from the backend

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const ShortUrl = require('./models/shortUrl');
 const bodyParser = require('body-parser');
 require('dotenv').config({ path: 'variables.env' });
 const PORT = process.env.PORT;
-var cors = require('cors')
+var cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -17,27 +17,24 @@ connectDB();
 
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find();
-    // console.log("shortUrl", shortUrls);
     res.send(shortUrls);
-})
-
-app.post('/shortUrls', async (req, res) => {
-    try {
-        await ShortUrl.create({ full: req.body.fullUrl });
-        res.sendStatus(200);
-    } catch (error) {
-        console.log(error.message);
-    }
-    res.redirect('/');
 });
 
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    res.redirect('http://localhost:3000');
+});
+
+
 app.get('/:shortUrl', async (req, res) => {
-    const shortUrl = ShortUrl.findOne({ short: req.params.shortUrl })
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
     if (shortUrl == null) return res.sendStatus(404);
 
     shortUrl.clicks++
+    shortUrl.last_access = new Date();
     shortUrl.save();
-})
+    res.redirect(shortUrl.full);
+});
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,19 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-// import SearchResults from './SearchResults';
-import axiosClient from '../config/axios'
-
-const icon = <FontAwesomeIcon icon={faSearch} />
-
-const submitUrl = async (e) => {
-    e.preventDefault();
-    await axiosClient.post('http://localhost:4000/shortUrls');
-}
-
+import axiosClient from '../config/axios';
+import moment from 'moment';
 
 function SearchBox() {
-    const [results, setResults] = useState("");
+    const [results, setResults] = useState([]);
+    const [shortUrl, setShortUrl] = useState("");
 
     useEffect(() => {
         async function getUrls() {
@@ -21,20 +12,32 @@ function SearchBox() {
                 const url = 'http://localhost:4000/'
                 const response = await axiosClient.get(url);
                 console.log("response.data", response.data);
-                setResults(response.data);
+                const responseData = response.data;
+                setResults(responseData);
             } catch (error) {
                 console.log(error.message);
             }
         }
         getUrls();
-    }, [])
+    }, [setResults]);
+
+    const getShortUrl = async (e) => {
+        const shortUrlCode = e.currentTarget.textContent;
+        try {
+            const url = `http://localhost:4000/${shortUrlCode}`
+            const response = await axiosClient.get(url);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return (
+
         <div className="searchbox">
             <h1 className="text-center pb-4">Post a URL</h1>
-            <form form action="/shortUrls" method="POST" className="search" onSubmit={submitUrl}>
-                <label for="fullUrl" className="sr-only">Url</label>
-                <input required placeholder="Url" type="url" name="fullUrl" id="fullUrl" className="form-control col mr-2" />
+            <form action="http://localhost:4000/shortUrls" method="POST" className="search">
+                <label htmlFor="fullUrl" className="sr-only">Url</label>
+                <input required placeholder="Please enter a URL here..." type="url" name="fullUrl" id="fullUrl" className="form-control col mr-2" />
                 <button className="searchButton " type="submit">
                     Shorten
                 </button>
@@ -46,21 +49,23 @@ function SearchBox() {
                     <tr>
                         <th>Full Url</th>
                         <th>Short Url</th>
-                        <th>registered_at</th>
-                        <th>last access</th>
+                        <th>Registered At</th>
+                        <th>Last Access</th>
                         <th>Clicks</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* {results.forEach(shortUrl => {
-                        <tr>
-                            <td><a href={shortUrl.full}>{shortUrl.full}</a></td>
-                            <td><a href={shortUrl.short}>{shortUrl.short}</a></td>
-                            <td>{shortUrl.registered_at}</td>
-                            <td>{shortUrl.last_access}</td>
-                            <td>clicks</td>
-                        </tr>
-                    })} */}
+                    {
+                        results?.map((result, index) => (
+                            <tr >
+                                <td ><a href={result.full}>{result.full}</a></td>
+                                <td ><a href={result.full}>{result.short}</a></td>
+                                <td >{moment(result.registered_at).format('DD/MM/YY')}</td>
+                                <td >{moment(result.last_access).format('DD/MM/YY')}</td>
+                                <td >{result.clicks}</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
